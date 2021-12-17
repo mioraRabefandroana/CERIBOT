81f0c70f67f764490b54c60eb4599a0141a2276a
rasa <miora.rabefandroana@alumni.univ-avignon.fr>
2021-12-17 15:16:19 +0100

    Simple générateur de html

---
+++ utils/custom_html.py
@@ -0,0 +1,6 @@
+
+
+def to_html(text):
+    html = '<!DOCTYPE html><html lang="fr"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>CERIBOT</title></head><body style="text-align: center;">{0}</body></html>'
+    html = html.format(text).replace("\n", "<br/>").replace("\\n", "<br/>")
+    return html
