76595f996d3d86f549fb5c306ccb71e3bdebb2ab
rasa <miora.rabefandroana@alumni.univ-avignon.fr>
2021-12-18 00:35:55 +0100

    reformatage des données de sortie text+html (fusionner par 27402740$)

--- utils/custom_html.py
+++ utils/custom_html.py
@@ -1,6 +1,9 @@
 
-
+messageGlue = "$$$$$"
 def to_html(text):
     html = '<!DOCTYPE html><html lang="fr"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>CERIBOT</title></head><body style="text-align: center;">{0}</body></html>'
     html = html.format(text).replace("\n", "<br/>").replace("\\n", "<br/>")
     return html
+
+def custom_response_message(text, html):
+    return "{0}$$$$${1}".format(text,html)
