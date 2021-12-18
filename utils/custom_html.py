2e1fdaa83e6b1806638d71cdde17e3dc0ff15369
rasa <miora.rabefandroana@alumni.univ-avignon.fr>
2021-12-18 01:06:15 +0100

    Ajout de css

--- utils/custom_html.py
+++ utils/custom_html.py
@@ -1,7 +1,7 @@
 
 messageGlue = "$$$$$"
 def to_html(text):
-    html = '<!DOCTYPE html><html lang="fr"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>CERIBOT</title></head><body style="text-align: center;">{0}</body></html>'
+    html = '<!DOCTYPE html><html lang="fr"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><link href="css/style.css" rel="stylesheet"><title>CERIBOT</title></head><body>{0}</body></html>'
     html = html.format(text).replace("\n", "<br/>").replace("\\n", "<br/>")
     return html
 
