654ff8dd29efdf86741649b96eb8b2eb18fcc3c3
rasa <miora.rabefandroana@alumni.univ-avignon.fr>
2022-01-05 01:49:25 +0100

    Quizz : implémentation du quizz (une simple question

--- utils/custom_messages.py
+++ utils/custom_messages.py
@@ -5,3 +5,6 @@ NO_CLASSROOM_AVAILABLE = "Navré, il n'y pas de salle disponible pour ce crénau
 NOT_FOUND_EDT = "Je n'ai pas pu trouver votre emploi du temps."
 NOT_FOUND_FORMATION = "Désolé, je ne retrouve pas la formation"
 NOT_FOUND_NIVEAU = "Désolé, je ne retrouve pas votre niveau de formation"
+QUIZZ_NOTICE = "Répondez par 1, ou 2, ou 3, ou 4."
+QUIZZ_TRUE = "Bravo, Vous avez répondu JUSTE."
+QUIZZ_FALSE = "FAUX."
